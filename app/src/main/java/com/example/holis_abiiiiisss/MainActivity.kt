5f9c8fae05d5b8443f244a418ac7f5b5fdfc697a
abi-2002-qw <abi.alavardo2210@gmail.com>
2024-11-21 22:00:16 -0500

    no ver

---
+++ app/src/main/java/com/example/holis_abiiiiisss/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.holis_abiiiiisss
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.holis_abiiiiisss.ui.theme.Holis_abiiiiisssTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Holis_abiiiiisssTheme { 
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Holis_abiiiiisssTheme {
+        Greeting("Android")
+    }
+}
